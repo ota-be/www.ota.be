59b46640f01b74bda597a4525f4c6721820c161a
Jan Vanhercke <jan.vanhercke@novotea.com>
2020-02-02 15:39:37 +0100

    new site

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,79 +1,60 @@
+$(window).load(function() {
+    $("#preloader").fadeOut("slow");
+});
 
-(function ($) {
-    "use strict";
+$(document).ready(function(){
 
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+     new WOW().init();
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
 
-        return check;
+     $('#top-nav').onePageNav({
+        currentClass: 'current',
+        changeHash: true,
+        scrollSpeed: 1200
     });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-
+     
+    //animated header class
+    $(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+     //console.log(scroll);
+    if (scroll > 200) {
+        //console.log('a');
+        $(".navigation").addClass("animated");
+    } else {
+        //console.log('a');
+        $(".navigation").removeClass("animated");
+    }});
     
-    
-   /*==================================================================
-    [ Simple slide100 ]*/
-
-    $('.simpleslide100').each(function(){
-        var delay = 7000;
-        var speed = 1000;
-        var itemSlide = $(this).find('.simpleslide100-item');
-        var nowSlide = 0;
-
-        $(itemSlide).hide();
-        $(itemSlide[nowSlide]).show();
-        nowSlide++;
-        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-
-        setInterval(function(){
-            $(itemSlide).fadeOut(speed);
-            $(itemSlide[nowSlide]).fadeIn(speed);
-            nowSlide++;
-            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-        },delay);
+    $year = $('#countdown_dashboard').data('year');
+    $month = $('#countdown_dashboard').data('month');
+    $day = $('#countdown_dashboard').data('day');
+    $('#countdown_dashboard').countDown({
+        targetDate: {
+            'day':      $day,
+            'month':    $month,
+            'year':     $year,
+            'hour':     23,
+            'min':      59,
+            'sec':      59,
+        },
+        omitWeeks: true
     });
 
+    $(".about-slider").owlCarousel(
+        {
+        singleItem: true,
+        pagination : true,
+        autoPlay : 5000,
+        }
+    );
 
-})(jQuery);
+});
+
+
+
+
+
+
+
